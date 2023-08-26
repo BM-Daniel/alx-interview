54ad098725f0063f2912c5d1f46c9c4efc4ed9e3
BM-Daniel <danbabereyir@gmail.com>
2023-08-26 17:45:29 +0000

    Pycodestyle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,18 +1,22 @@
+#!/usr/bin/python3
+import math
+
+
 def pascal_triangle(n):
-    import math
     result = []
 
     if n <= 0:
         return result
-    
+
     for row in range(0, n):
         rowArray = []
 
         for value in range(0, row + 1):
-            binomial = int(math.factorial(row) / (math.factorial(value) * math.factorial(row - value)))
+            binomial = int(math.factorial(row) /
+                           (math.factorial(value) *
+                            math.factorial(row - value)))
             rowArray.append(binomial)
-        
+
         result.append(rowArray)
-        
+
     return result
-    
