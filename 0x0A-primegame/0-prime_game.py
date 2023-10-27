78f4d8abbbfd25d158994eb6870e5d632ffc23dc
BM-Daniel <danbabereyir@gmail.com>
2023-10-27 10:25:28 +0000

    Fix issue

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -9,14 +9,11 @@ cannot make a move loses the game.
 
 
 def isWinner(x, nums):
-    '''
-    Function for prime game
-    '''
+    '''finds the winner'''
     winnerCounter = {'Maria': 0, 'Ben': 0}
 
     for i in range(x):
         roundWinner = isRoundWinner(nums[i], x)
-
         if roundWinner is not None:
             winnerCounter[roundWinner] += 1
 
@@ -29,26 +26,27 @@ def isWinner(x, nums):
 
 
 def isRoundWinner(n, x):
-    '''
-    Function to round winner
-    '''
+    '''find round winner'''
     list = [i for i in range(1, n + 1)]
     players = ['Maria', 'Ben']
 
     for i in range(n):
+        # get current player
         currentPlayer = players[i % 2]
         selectedIdxs = []
         prime = -1
-
         for idx, num in enumerate(list):
+            # if already picked prime num then
+            # find if num is multipl of the prime num
             if prime != -1:
                 if num % prime == 0:
                     selectedIdxs.append(idx)
+            # else check is num is prime then pick it
             else:
                 if isPrime(num):
                     selectedIdxs.append(idx)
                     prime = num
-
+        # if failed to pick then current player lost
         if prime == -1:
             if currentPlayer == players[0]:
                 return players[1]
@@ -61,14 +59,14 @@ def isRoundWinner(n, x):
 
 
 def isPrime(n):
-    '''
-    Function for other conditions
-    '''
+    # 0, 1, even numbers greater than 2 are NOT PRIME
     if n == 1 or n == 0 or (n % 2 == 0 and n > 2):
         return False
     else:
+        # Not prime if divisable by another number less
+        # or equal to the square root of itself.
+        # n**(1/2) returns square root of n
         for i in range(3, int(n**(1/2))+1, 2):
             if n % i == 0:
                 return "Not prime"
-
         return True
